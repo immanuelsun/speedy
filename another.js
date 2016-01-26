f64c0bb5643e6fef4b53c99fd6c58beaa14611c5
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2016-01-25 18:28:17 -0800

    Make it so

---
+++ another.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    var Person = (function() {
+        function Person() {
+
+        }
+        Person.prototype = {
+
+        };
+
+        return Person;
+    }());
+}());
